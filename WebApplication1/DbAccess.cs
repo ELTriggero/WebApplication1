adb7f68c5863b688a9cd3aea3a280fa707251ed2
ELTriggero <mirco.barison97@gmail.com>
2019-07-12 17:13:57 +0200

    Aggiungere i file di progetto.

---
+++ WebApplication1/DbAccess.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication1
+{
+    public class DbAccess
+    {
+        private static SqlConnection connection = new SqlConnection();
+        private static SqlCommand command = new SqlCommand();
+        private static SqlDataReader DbReader;
+        private static SqlDataAdapter adapter = new SqlDataAdapter();
+        public SqlTransaction DbTran;
+
+        private static string strConnString = "";
+
+        public int executeDataAdapter(DataTable tblName, string strSelectSql)
+        {
+            try
+            {
+                if (connection.State == 0)
+                {
+                    createConn();
+                }
+                adapter.SelectCommand.CommandText = strSelectSql;
+                adapter.SelectCommand.CommandType = CommandType.Text;
+                SqlCommandBuilder DbCommandBuilder = new SqlCommandBuilder(adapter);
+                DbCommandBuilder.ConflictOption = ConflictOption.OverwriteChanges;
+
+                return adapter.Update(tblName);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public int AliExecuteDataAdapter(DataTable tblName, string strSelectSql)
+        {
+            try
+            {
+                if (connection.State == 0)
+                {
+                    createConn();
+                }
+                adapter.SelectCommand.CommandText = strSelectSql;
+                adapter.SelectCommand.CommandType = CommandType.Text;
+                SqlCommandBuilder DbCommandBuilder = new SqlCommandBuilder(adapter);
+                DbCommandBuilder.ConflictOption = ConflictOption.OverwriteChanges;
+
+                string inser = DbCommandBuilder.GetInsertCommand().ToString();
+                string update = DbCommandBuilder.GetUpdateCommand().CommandText.ToString();
+                string delete = DbCommandBuilder.GetDeleteCommand().CommandText.ToString();
+
+                return adapter.Update(tblName);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void beginTrans()
+        {
+            try
+            {
+                if (DbTran == null)
+                {
+                    if (connection.State == 0)
+                    {
+                        createConn();
+                        DbTran = connection.BeginTransaction();
+                        adapter.SelectCommand.Transaction = DbTran;
+                    }
+                    else
+                    {
+
+                        DbTran = connection.BeginTransaction();
+                        adapter.SelectCommand.Transaction = DbTran;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void commitTrans()
+        {
+            try
+            {
+                if (DbTran != null)
+                {
+                    DbTran.Commit();
+                    DbTran = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void rollbackTrans()
+        {
+            try
+            {
+                if (DbTran != null)
+                {
+                    DbTran.Rollback();
+                    DbTran = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void createConn()
+        {
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.ConnectionString = strConnString;
+                    connection.Open();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void closeConn()
+        {
+            connection.Close();
+
+        }
+
+        public void readDatathroughAdapter(string query, DataTable tblName)
+        {
+            try
+            {
+                if (connection.State == ConnectionState.Closed)
+                {
+                    createConn();
+                }
+                command.Connection = connection;
+                command.CommandText = query;
+                command.CommandType = CommandType.Text;
+
+                adapter = new SqlDataAdapter(command);
+                adapter.Fill(tblName);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public SqlDataReader readDatathroughReader(string query)
+        {
+            //DataReader used to sequentially read data from a data source
+            SqlDataReader reader;
+            try
+            {
+                if (connection.State == ConnectionState.Closed)
+                {
+                    createConn();
+                }
+                command.Connection = connection;
+                command.CommandText = query;
+                command.CommandType = CommandType.Text;
+
+                reader = command.ExecuteReader();
+                return reader;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public int executeQuery(SqlCommand dbCommand)
+        {
+            try
+            {
+                if (connection.State == ConnectionState.Closed)
+                {
+                    createConn();
+                }
+                dbCommand.Connection = connection;
+                dbCommand.CommandType = CommandType.Text;
+
+                return dbCommand.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ WebApplication1/WebService1.asmx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using Newtonsoft.Json;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication1
+{
+    /// <summary>
+    /// Descrizione di riepilogo per WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Per consentire la chiamata di questo servizio Web dallo script utilizzando ASP.NET AJAX, rimuovere il commento dalla riga seguente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+        DataTable dtCountries = new DataTable();
+        DbAccess objDbAccess = new DbAccess();
+        DataTable dtusers = new DataTable();
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public int Sum(int a, int b)
+        {
+            return a+b;
+        }
+
+        [WebMethod]
+        public string Countries()
+        {
+            dtCountries.Columns.Add("CountryName");
+            dtCountries.Columns.Add("Continent");
+
+            dtCountries.Rows.Add("Pakistan", "Asia");
+            dtCountries.Rows.Add("india", "Asia");
+            dtCountries.Rows.Add("Germany", "Europe");
+            dtCountries.Rows.Add("Srilanka", "Asia");
+            dtCountries.Rows.Add("Australia", "Australia");
+            dtCountries.Rows.Add("New Zeland", "Austrlia");
+
+            return JsonConvert.SerializeObject(dtCountries);
+        }
+
+        [WebMethod]
+        public string dataTablesForusers(string id)
+        {
+            string query = "Select * From Users Where ID = '" + id + "'";
+            objDbAccess.readDatathroughAdapter(query, dtusers);
+
+            string result = JsonConvert.SerializeObject(dtusers);
+            return result;
+        }
+    }
+}
